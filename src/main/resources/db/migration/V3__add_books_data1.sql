6082a5b84fc768e8b6c723d3c48420197c29e6f2
Murukesh Radhakrishnan <rmurukesh@gmail.com>
2024-12-23 18:03:33 -0600

    initial commit

---
+++ src/main/resources/db/migration/V3__add_books_data1.sql
@@ -0,0 +1,2 @@
+insert into products(code, name, description, image_url, price) values
+('P115','To Kill a Mockingbird','The unforgettable novel of a childhood in a sleepy Southern town and the crisis of conscience that rocked it...','https://images.gr-assets.com/books/1361975680l/2657.jpg', 45.40);
